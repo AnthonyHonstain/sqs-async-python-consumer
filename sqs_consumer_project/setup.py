e46c7d7e3be44c96716a11168fab797c102598f4
Anthony Honstain <honstain@gmail.com>
2024-03-16 08:34:48 -0700

    Increase the logging on HTTPX

--- sqs_consumer_project/setup.py
+++ sqs_consumer_project/setup.py
@@ -4,6 +4,8 @@ from pythonjsonlogger import jsonlogger
 
 def setup_logging():
     log_level = logging.INFO  # or any other level
+    httpx_log_level = logging.DEBUG  # For detailed connection logs
+
     logger = logging.getLogger()
     logger.setLevel(log_level)
 
@@ -11,3 +13,10 @@ def setup_logging():
     formatter = jsonlogger.JsonFormatter("%(asctime)s %(name)s %(levelname)s %(message)s")
     log_handler.setFormatter(formatter)
     logger.addHandler(log_handler)
+
+    # Configure HTTPX and httpcore logging
+    httpx_logger = logging.getLogger("httpx")
+    httpx_logger.setLevel(httpx_log_level)
+
+    httpcore_logger = logging.getLogger("httpcore")
+    httpcore_logger.setLevel(httpx_log_level)
